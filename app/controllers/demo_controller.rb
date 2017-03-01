dc4344d9227244260c0a82bda1220a7d2303bf46
John Stegman <john@green-leaf-services.com>
2017-03-01 16:42:22 -0500

    After Section 3

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,24 @@
 class DemoController < ApplicationController
-    layout false    
-    def index
+    layout false   
+    
+    def index #this is an action
+        render('index') #the default action - does not need to be specified
+        #render('hello') #sets the template for the index action to hello
+    end
+    
+    def hello
+        @array = [1,2,3,4,5]
+        @id = params['id']
+        @page = params[:page]
+        render('hello') #the default action - does not need to be specified
+        #render('index')  #sets the template for the hello action to index
+    end
+    
+    def other_hello
+        redirect_to(:action => 'hello')
+    end
+    
+    def lynda
+        redirect_to('http://lynda.com')
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
-    root 'march1#index2'
+    root 'demo#index'
     get 'demo/index'
     get 'march1/index2'
     get 'march1/index3'
+    get 'demo/hello'
+    get 'demo/other_hello'
+    get 'demo/lynda'
+    
     
     # default route
     # may go away in future versions of Rails 
