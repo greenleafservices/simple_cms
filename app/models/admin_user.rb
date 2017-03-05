b94aceff09764cd67cb868238f17b9dd1819dfd4
John Stegman <john@green-leaf-services.com>
2017-03-05 16:01:46 -0500

    Prior to Reset

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,6 @@
-class Admin_User < ApplicationRecord
+class AdminUser < ApplicationRecord
+
+  has_and_belongs_to_many :pages
+
   scope :new_first, lambda { order("created_at DESC") }
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,7 @@ class Page < ApplicationRecord
 
     belongs_to :subject
     has_many :sections
+    has_and_belongs_to_many :admin_users
 
   scope :visible, lambda { where(:visible => true) }
   scope :invisible, lambda { where(:visible => false) }
