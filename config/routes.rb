62e920eefd14adee7b128240d085c7b82f3c89db
John Stegman <john@green-leaf-services.com>
2017-03-06 15:33:17 -0500

    After Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,32 @@
 Rails.application.routes.draw do
-  
+
+# These match routes (defaults) were commented in lesson 07-03
+
+  #get 'subjects/index'
+  #get 'subjects/show'
+  #get 'subjects/new'
+  #get 'subjects/edit'
+  #get 'subjects/delete'
+
     root 'demo#index'
+    resources :subjects do # default
+      member do
+        get :delete #should be added ,no part of default resources
+      end
+    end
+
     get 'demo/index'
     get 'march1/index2'
     get 'march1/index3'
     get 'demo/hello'
     get 'demo/other_hello'
     get 'demo/lynda'
-    
-    
+
+
+
     # default route
-    # may go away in future versions of Rails 
+    # may go away in future versions of Rails
     # get ':controller(/:action(/:id))'
-  
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
