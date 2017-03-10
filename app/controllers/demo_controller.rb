a18997d4d2ebec69e085286e36aa61657a0b1150
John Stegman <john@green-leaf-services.com>
2017-03-10 10:15:37 -0500

    After Chapter 9

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,11 +1,11 @@
 class DemoController < ApplicationController
-    layout false   
-    
+    layout false
+
     def index #this is an action
         render('index') #the default action - does not need to be specified
         #render('hello') #sets the template for the index action to hello
     end
-    
+
     def hello
         @array = [1,2,3,4,5]
         @id = params['id']
@@ -13,12 +13,15 @@ class DemoController < ApplicationController
         render('hello') #the default action - does not need to be specified
         #render('index')  #sets the template for the hello action to index
     end
-    
+
     def other_hello
         redirect_to(:action => 'hello')
     end
-    
+
     def lynda
         redirect_to('http://lynda.com')
+    end
+
+    def escape_output
     end
 end
